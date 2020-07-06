7509031a64bbd2570ce7ceca9019454a283fcc29
Christian Gomez Martinez <gomez9289@gmail.com>
2020-07-06 13:38:41 -0400

    home page layout

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,25 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 class HomePage extends Component {
   render() {
     return (
-      <div className='Home'>
-        <div className='row'>
-          <div className='col'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/pages/Home.js</code> and save to reload.
-            </p>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
+      <div className="Home">
+        <div className="row">
+          <div className="col">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+            <div className="card text-center">
+            <img src="https://www.altran.com/as-content/uploads/sites/4/2017/05/5-0_finance_1600.jpg" class="card-img-top" alt="..."/>
+              <div className="card-header">Featured</div>
+              <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+                  <p></p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+
+            <p></p>
+
+            <div className="row row-cols-3 row-cols-md-3">
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col mb-4">
+    <div className="card">
+      <img src="..." class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
           </div>
         </div>
       </div>
