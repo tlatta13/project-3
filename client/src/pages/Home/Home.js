07e27871f3acc69f5ed0b484d56fa7bc816d3b7d
Christian Gomez Martinez <gomez9289@gmail.com>
2020-07-07 10:23:21 -0400

    card tittles and shadow effect

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,18 +7,17 @@ class HomePage extends Component {
       <div className="Home">
         <div className="row">
           <div className="col">
-
             <img
               src="https://www.altran.com/as-content/uploads/sites/4/2017/05/5-0_finance_1600.jpg"
-              class="card-img-top"
+              className="card-img-top shadow"
               alt="..."
             />
 
             <div className="card text-center my-4">
-              <div className="card-header">
+              <div className="card-header shadow">
                 <h2>Budget App</h2>
               </div>
-              <div className="card-body">
+              <div className="card-body shadow">
                 <p className="card-text">
                   With supporting text below as a natural lead-in to additional
                   content.
@@ -34,38 +33,54 @@ class HomePage extends Component {
 
             <div className="row row-cols-3 row-cols-md-3">
               <div className="col mb-4">
-                <div className="info-card bg-light">
+                <div className="info-card bg-light shadow">
                   <img src="..." className="card-img-top mt-3" alt="..." />
                   <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Manage Spending</h5>
+                    <p className="card-text">
+                      This is a longer card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
                   </div>
                 </div>
               </div>
               <div className="col mb-4">
-                <div className="info-card bg-light">
+                <div className="info-card bg-light shadow">
                   <img src="..." className="card-img-top mt-3" alt="..." />
                   <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Plan Savings</h5>
+                    <p className="card-text">
+                      This is a longer card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
                   </div>
                 </div>
               </div>
               <div className="col mb-4">
-                <div className="info-card bg-light">
+                <div className="info-card bg-light shadow">
                   <img src="..." className="card-img-top mt-3" alt="..." />
                   <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This card is a little bit longer.</p>
+                    <h5 className="card-title">View Summaries</h5>
+                    <p className="card-text">
+                      This is a longer card with supporting text below as a
+                      natural lead-in to additional content. This card is a
+                      little bit longer.
+                    </p>
                   </div>
                 </div>
               </div>
               <div className="col mb-4">
-                <div className="info-card bg-light">
+                <div className="info-card bg-light shadow">
                   <img src="..." class="card-img-top mt-3" alt="..." />
                   <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 className="card-title">Graphs and Reports</h5>
+                    <p className="card-text">
+                      This is a longer card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
                   </div>
                 </div>
               </div>
