16abb6eef1371f86fbd0505cbdcf49a60c8a1643
Bourbon Man <brettrushing1215@gmail.com>
2020-07-07 13:38:09 -0400

    add a grid to the dashboard page so we can insert a graph

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,24 +1,32 @@
 import React, { useContext } from "react";
-import AuthContext from '../../contexts/AuthContext'
+import AuthContext from "../../contexts/AuthContext";
 const Dashboard = () => {
-   const userInfo = useContext(AuthContext)
+  const userInfo = useContext(AuthContext);
 
-   const style ={
-       buttons:{
-           'display':'flex',
-           'justifyContent':'center'
-       }
-   }
-  return <>
-    <h1 className='text-center text-light my-3'>Welcome {userInfo.user ? userInfo.user.email:null}! </h1>
-    <div style={style.buttons}>
-
-   <button className='btn btn-success'>Add Income</button>
-   <div style ={{'width':'10%'}}/>
-   <button className='btn btn-success'>Add Expense</button>
-    </div>
-  
-  </>;
+  const style = {
+    buttons: {
+      display: "flex",
+      justifyContent: "center",
+    },
+  };
+  return (
+    <>
+      <h1 className="text-center text-light my-3">
+        Welcome {userInfo.user ? userInfo.user.email : null}!{" "}
+      </h1>
+      <div style={style.buttons}>
+        <button className="btn btn-success">Add Income</button>
+        <div style={{ width: "10%" }} />
+        <button className="btn btn-success">Add Expense</button>
+      </div>
+      <div className="container">
+        <div class="row">
+          <div class="col-sm d-flex flex-column justify-content-center" style={{height:500}}>Insert Graph</div>
+          <div class="col-sm d-flex flex-column justify-content-center" style={{height:500}}>Insert Graph</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
