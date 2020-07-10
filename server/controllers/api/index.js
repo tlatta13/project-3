0649068047db5a61e15f6566ebb2e69a4912615b
Tom Savage <tsavage@gmail.com>
2020-07-10 18:06:43 -0400

    charts 3

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -4,11 +4,6 @@ apiControllers.use('/users', require('./usersController'));
 apiControllers.use('/secrets', require('./secretsController'));
 apiControllers.use('/income', require('./incomeController'));
 apiControllers.use('/expense', require('./expenseController'));
-<<<<<<< HEAD
-
 apiControllers.use('/savings', require('./savingsController'));
-=======
-apiControllers.use('/savings', require('./expenseController'));
->>>>>>> 193aa089056c82cc88cc06fd5bea1f115cf71730
 apiControllers.use('/stock', require('./stockController'));
 module.exports = apiControllers;
