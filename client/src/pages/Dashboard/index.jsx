99b92c28f6ef37789da0df409685b4643eaf7a02
Christian Gomez Martinez <gomez9289@gmail.com>
2020-07-13 14:29:14 -0400

    dashboard

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -5,15 +5,13 @@ import Income from "../../components/Income";
 import Expense from "../../components/Expense";
 import MonthlyBar from "../../components/Charts/monthlybar";
 import PieChart from "../../components/Charts/piechart";
-import IncomeTable from "../../components/Tables/income"
-import ExpenseTable from "../../components/Tables/expense"
-import SavingsTable from "../../components/Tables/savings"
+import IncomeTable from "../../components/Tables/income";
+import ExpenseTable from "../../components/Tables/expense";
+import SavingsTable from "../../components/Tables/savings";
 import Savings from "../../components/Savings";
-
 
 const customStyles = {
   content: {
-
     top: "50%",
     left: "50%",
     right: "auto",
@@ -21,10 +19,9 @@ const customStyles = {
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
   },
-  main:{
-      'fontFamily': 'Arial, Helvetica, sans-serif'
-    
-  }
+  main: {
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
 };
 const Dashboard = () => {
   // Modal Info Start
@@ -43,7 +40,7 @@ const Dashboard = () => {
   const closeModal = () => {
     setIsOpen(false);
   };
-  
+
   const userInfo = useContext(AuthContext);
 
   const style = {
@@ -52,11 +49,11 @@ const Dashboard = () => {
       justifyContent: "center",
     },
     incomeButton: {
-      backgroundColor: "green"
+      backgroundColor: "green",
     },
     expenseButton: {
-      backgroundColor: "red"
-    }
+      backgroundColor: "red",
+    },
   };
   // Modal Info End
 
@@ -64,28 +61,31 @@ const Dashboard = () => {
     <div className="class" style={customStyles.main}>
       <div className="container bg-light border-0 rounded my-4">
         <h3 className="text-center text-light mb-3 pt-4">
-          <div style={{color:"black"}}>
-          Welcome {userInfo.user ? userInfo.user.email : null}!{" "}
+          <div style={{ color: "black" }}>
+            Welcome {userInfo.user ? userInfo.user.email : null}!{" "}
           </div>
         </h3>
         <div style={style.buttonsDiv}>
           <button
             style={style.incomeButton}
             className="income btn btn-secondary mr-1 mb-4"
-            onClick={() => openModal("income")}>
-              Add Income
+            onClick={() => openModal("income")}
+          >
+            Add Income
           </button>
-          <button 
+          <button
             style={style.incomeButton}
-            className="expense btn btn-secondary mx-2 mb-4" 
-            onClick={()=>openModal('savings')}>
-              Add Savings
+            className="expense btn btn-secondary mx-2 mb-4"
+            onClick={() => openModal("savings")}
+          >
+            Add Savings
           </button>
-          <button 
+          <button
             style={style.expenseButton}
-            className="expense btn btn-secondary ml-1 mb-4" 
-            onClick={()=>openModal('expense')}>
-              Add Expense
+            className="expense btn btn-secondary ml-1 mb-4"
+            onClick={() => openModal("expense")}
+          >
+            Add Expense
           </button>
         </div>
       </div>
@@ -97,7 +97,7 @@ const Dashboard = () => {
             className="col-sm d-flex flex-column justify-content-center my-3"
             style={{ height: 500 }}
           >
-             <MonthlyBar />
+            <MonthlyBar />
           </div>
           <div
             className="col-sm d-flex flex-column justify-content-center my-3"
@@ -107,43 +107,39 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      
+
       {/* Modals */}
-      <Modal Income
+      <Modal
+        Income
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={customStyles}
         contentLabel="Income Modal"
       >
-        {modalContent === "income" ? <Income close={closeModal} /> :
-         modalContent === "expense" ? <Expense close={closeModal} />:
-         <Savings close={closeModal}/> }
-        </Modal>
+        {modalContent === "income" ? (
+          <Income close={closeModal} />
+        ) : modalContent === "expense" ? (
+          <Expense close={closeModal} />
+        ) : (
+          <Savings close={closeModal} />
+        )}
+      </Modal>
 
       {/* Income Savings and Expense Tables */}
       <div className="container bg-light border-0 rounded my-4">
-        <h3 className="text-center py-4">
-          Income
-        </h3>
-        <IncomeTable
-        />
+        <h3 className="text-center py-4">Income</h3>
+        <IncomeTable />
       </div>
 
       <div className="container bg-light border-0 rounded my-4">
-        <h3 className="text-center mb-3 py-4">
-          Savings
-        </h3>
-        <SavingsTable
-        />
+        <h3 className="text-center mb-3 py-4">Savings</h3>
+        <SavingsTable />
       </div>
 
       <div className="container bg-light border-0 rounded my-4">
-        <h3 className="text-center mb-3 py-4">
-          Expenses
-        </h3>
-        <ExpenseTable
-        />
+        <h3 className="text-center mb-3 py-4">Expenses</h3>
+        <ExpenseTable />
       </div>
     </div>
   );
